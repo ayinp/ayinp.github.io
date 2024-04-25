905bf12221755ef9da263c800522e27c7b8cce04
Ayin <sourlemon2270@gmail.com>
2024-04-24 22:46:43 -0400

    autism simulator

--- index.js
+++ index.js
@@ -101,136 +101,176 @@ var social;
 var sleep;
 var index;
 
+var poemMon = 
+"My expectations for today <br>" + 
+"are not complete without <br>"+
+"the dread awaiting my week";
+var poemTues = 
+"By the end of today <br>" + 
+"my bones will hurt, <br>" + 
+"with all of my energy depleated. <br>" +
+"Despite the exhaustion, <br>" + 
+"I won't forget to make an effort. <br>" +
+"Like always.";
+var poemWed = 
+"Waking up today is always <br>" +
+"harder than I expect. <br>"+
+"I sleep in despite knowing that <br>"+
+"I should not. <br>"+
+"Although the week is half over, <br>"+
+"I still need to make it to the weekend. <br>"+
+"I have so much left to do <br>"+
+"on my list. <br>";
+var poemThurs = 
+"This is the hardest day of the week. <br>"+
+"This is the day I underestimate. <br>"+
+"And I realize how tired I am. <br>";
+var poemFri = 
+"My obligations <br>"+
+"are not yet complete, <br>"+
+"Even though I long <br>"+
+"To do nothing <br>";
+var poemSat = 
+"Today I focus on my hobbies. <br>"+
+"Today I focus on my self care. <br>"+
+"Today I focus on all the little things <br>"+
+"That make me human. <br>"+
+"Today I focus on me. <br>";
+var poemSun = 
+"The week is over, <br>"+
+"time to breathe. <br>"+
+" <br>"+
+"For now. <br>";
+
 const Scenes = [
-    
     //MONDAY grades sanity social energy
-    new MyScene("/source/art1.png",
+    new MyScene(poemMon,
     "Happy Monday! \n You wake up to a blaring alarm. You have 3 classes this morning that you are dreading. Your friends are all studying together in a lounge. What would you like to do this morning?",
     [new MyOption("attend classes",[[5,5,5],-5,0,-5]),
     new MyOption("study with friends",[[2,2,2],-5,5,-5]), 
     new MyOption("sleep in and skip class",[[-2,-2,-2],5,0,5])]),
     
-    new MyScene("/source/art2.png",
+    new MyScene(poemMon,
     "Your friends want to meet up for lunch, but youre feeling a bit overwhelmed. What should you do?",
     [new MyOption("go see them!",[[0,0,0],-5,15,-5]),
     new MyOption("take a nap",[[0,0,0],5,-10,5]), 
     new MyOption("use the time to recharge alone",[[0,0,0],15,-10,0])]),
 
-    new MyScene("/source/art3.png",
+    new MyScene(poemMon,
     "You have been waiting to play a new video game for ages, but your friends want to party. what are the plans for tonight?",
     [new MyOption("Stay in and play video games",[[0,0,0],15,-5,-5]),
     new MyOption("go to bed early",[[0,0,0],5,-15,15]), 
     new MyOption("attend the party",[[0,0,0],-5,15,-5])]),
 
     //TUESDAY  grades health social sleep
-    new MyScene("/source/art4.png",
+    new MyScene(poemTues,
     "You have no classes until this afternoon, how would you like to start the day?",
     [new MyOption("go back to sleep",[[0,0,0],5,0,10]),
     new MyOption("meet up with friends",[[0,0,0],-5,15,0]), 
     new MyOption("review class notes",[[5,5,5],0,0,-5])]),
 
-    new MyScene("/source/art5.png",
+    new MyScene(poemTues,
     "Youve got 3 classes this afternoon, but your friends want to go to the redsocks game!",
     [new MyOption("go to classes",[[5,5,5],0,-10,0]),
     new MyOption("take a nap",[[0,0,0],5,-15,15]), 
     new MyOption("go to the redsocks game",[[-2,-2,-2],-5,15,0])]),
 
-    new MyScene("/source/art6.png",
+    new MyScene(poemTues,
     "you feel exhausted this evening, but luckily there isn't much going on.",
     [new MyOption("go to bed early",[[0,0,0],0,0,10]),
     new MyOption("recharge with some of your favorite hobbys",[[0,0,0],10,0,0]), 
     new MyOption("practice some self care",[[0,0,0],10,0,0])]),
  
     //WEDNESDAY  grades health social sleep
-    new MyScene("/source/art7.png",
+    new MyScene(poemWed,
     "you wake up today to the sounds of rain. You have three classes this morning, but getting outside will be particularly difficult.",
     [new MyOption("go to classes",[[5,5,5],-15,0,0]),
     new MyOption("study inside",[[2,2,2],0,0,-5]), 
     new MyOption("go back to sleep",[[-2,-2,-2],10,0,5])]),
 
-    new MyScene("/source/art8.png",
+    new MyScene(poemWed,
     "The rain has let up, and your friends want to go into the city. Are you joining?",
     [new MyOption("Stay home just in case",[[0,0,0],10,-10,0]),
     new MyOption("go out and enjoy the day",[[0,0,0],-10,10,0]), 
     new MyOption("take a nap",[[0,0,0],0,-10,10])]),
 
-    new MyScene("/source/art9.png",
+    new MyScene(poemWed,
     "You have date-night with your boyfriend planned, but you forgot about a very important Chem homework due tonight!",
     [new MyOption("cancel date night and do your work",[[0,15,0],0,-15,0]),
     new MyOption("forget about the work and just have fun",[[0,-15,0],-5,15,0]), 
     new MyOption("stay in and cry because you messed up",[[0,-15,0],10,-15,0])]),
 
     //THURSDAY  grades health social sleep
-    new MyScene("/source/art10.png",
+    new MyScene(poemThurs,
     "You have no classes until this afternoon, but you should probably study. What will you do?",
     [new MyOption("sleep in",[[0,0,0],0,0,10]),
     new MyOption("study",[[5,5,5],0,0,0]), 
     new MyOption("take a long shower",[[0,0,0],10,0,0])]),
 
-    new MyScene("/source/art11.png",
+    new MyScene(poemThurs,
     "You decided to go to classes this afternoon, but the T was packed on the way there, which made you very overwhelmed!",
     [new MyOption("try to go to classes anyway",[[5,5,5],-15,0,0]),
     new MyOption("go back home to recover",[[-2,-2,-2],19,0,0]), 
     new MyOption("go sit in the nearest building and do nothing until you feel better again",[[-2,-2,-2],5,0,0])]),
 
-    new MyScene("/source/art12.png",
+    new MyScene(poemThurs,
     "You're still overwhelmed from earlier, but your friends want you to attend thirsty thursday this week.",
     [new MyOption("Go to the party",[[0,0,0],-10,15,0]),
     new MyOption("go to bed early",[[0,0,0],5,-10,10]), 
     new MyOption("have a calm night at home",[[0,0,0],15,-10,0])]),
 
     //FRIDAY  grades health social sleep
-    new MyScene("/source/art13.png",
+    new MyScene(poemFri,
     "You have 3 classes this morning, and your boyfriend wants to get brunch. What will you do?",
     [new MyOption("go to brunch",[[-2,-2,-2],0,10,0]),
     new MyOption("go to class",[[5,5,5],0,0,0]), 
     new MyOption("go back to sleep",[[-2,-2,-2],5,-10,10])]),
 
-    new MyScene("/source/art14.png",
+    new MyScene(poemFri,
     "You feel like youre starting to come down with a cold, but it might just be allergies. You are very anxious about this.",
     [new MyOption("Spiral",[[0,0,0],-10,0,0]),
     new MyOption("take a nap",[[0,0,0],0,0,10]), 
     new MyOption("call a friend",[[0,0,0],5,5,0])]),
 
-    new MyScene("/source/art15.png",
+    new MyScene(poemFri,
     "You had a pretty bad afternoon, but you promised your friend you would be at his birthday party. What should you do?",
     [new MyOption("attend the party",[[0,0,0],0,10,0]),
     new MyOption("cancel and stay home",[[0,0,0],10,-10,0]), 
     new MyOption("go to bed early",[[0,0,0],0,-10,10])]),
 
     //SATURDAY  grades health social sleep
-    new MyScene("/source/art16.png",
+    new MyScene(poemSat,
     "A group of friends invited you to the froyo shop, but another group of friends invited you to watch a movie. What do you pick?",
     [new MyOption("go get froyo",[[0,0,0],-5,-5,0]),
     new MyOption("go to the movie",[[0,0,0],-5,-5,0]), 
     new MyOption("do nothing because this is too stressful :(",[[0,0,0],5,-10,0])]),
 
-    new MyScene("/source/art17.png",
+    new MyScene(poemSat,
     "You should get some studying done this afternoon, but you really don't feel like it.",
     [new MyOption("study anyway",[[5,5,5],-15,0,0]),
     new MyOption("take a nap",[[0,0,0],5,0,10]), 
     new MyOption("ignore your obligations",[[0,0,0],10,0,0])]),
 
-    new MyScene("/source/art18.png",
+    new MyScene(poemSat,
     "Your roomate wants to host a party at your dorm tonight on short notice. How will you deal with this?",
     [new MyOption("tell her not tonight",[[0,0,0],10,-15,0]),
     new MyOption("let her host, but stay in the lounge alone",[[0,0,0],5,-5,0]), 
     new MyOption("attend the event with everyone else",[[0,0,0],-10,10,0])]),
 
     //SUNDAY  grades health social sleep
-    new MyScene("/source/art19.png",
+    new MyScene(poemSun,
     "You wake up this morning with a splitting headache, but you agreed to get breakfast with people!",
     [new MyOption("go back to sleep",[[0,0,0],0,-10,10]),
     new MyOption("send a text canceling brunch",[[0,0,0],5,-5,0]), 
     new MyOption("attend anyway",[[0,0,0],-10,10,0])]),
 
-    new MyScene("/source/art20.png",
+    new MyScene(poemSun,
     "You still have a headache, and you really should study.",
     [new MyOption("Study anyway",[[5,5,5],0,0,0]),
     new MyOption("Take a shower",[[0,0,0],10,0,0]), 
     new MyOption("take a nap",[[0,0,0],0,0,10])]),
 
-    new MyScene("/source/art21.png",
+    new MyScene(poemSun,
     "Your friends want to see you tonight, but you are exhausted.",
     [new MyOption("get dinner with them",[[0,0,0],-5,10,0]),
     new MyOption("go to bed early",[[0,0,0],0,-5,10]), 
@@ -271,7 +311,7 @@ function updateAssignments(){
 function UpdateScene(scene){
     console.log("UPDATE SCENE");
     UpdateStats();
-    document.getElementById("art").innerHTML = "I will put a picture of a poem or art peice here";
+    document.getElementById("art").innerHTML = scene.art;
     document.getElementById("question").innerHTML = scene.question;
     document.getElementById("day").innerHTML = calcDay();
     UpdateOptions(scene);
